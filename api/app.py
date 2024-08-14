2c0e2b998c9860a1d44981a72076761dbbe4e221
Douglas Utz <douglas@utz.net.br>
2024-08-13 23:23:42 -0300

    Test

--- api/app.py
+++ api/app.py
@@ -1,8 +1,10 @@
+# Imports
 from flask import Flask, render_template, jsonify
 from urllib.parse import unquote
-# from utils.http_request import *
-# from utils.get_datetime import *
+from utils.http_request import *
+from utils.get_datetime import *
 
+# Create Flask app
 app = Flask(__name__)
 
 # Home
@@ -10,24 +12,24 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# # Track product endpoint
-# @app.route('/track/<path:url>', methods=['GET'])
-# def get_product(url):
+# Track product endpoint
+@app.route('/track/<path:url>', methods=['GET'])
+def get_product(url):
     
-#     # Decode the URL
-#     product_url = unquote(url)
+    # Decode the URL
+    product_url = unquote(url)
     
-#     # Get the necessary data
-#     name = get_name(product_url)
-#     price = get_price(product_url)
-#     date = get_date()
-#     time = get_time()
+    # Get the necessary data
+    name = get_name(product_url)
+    price = get_price(product_url)
+    date = get_date()
+    time = get_time()
     
-#     # Create the dictionary (JSON)
-#     product = {
-#         'name': str(name),
-#         'price': str(price),
-#         'date': date,
-#         'time': time
-#     }
-#     return jsonify(product)
+    # Create the dictionary (JSON)
+    product = {
+        'name': str(name),
+        'price': str(price),
+        'date': date,
+        'time': time
+    }
+    return jsonify(product)

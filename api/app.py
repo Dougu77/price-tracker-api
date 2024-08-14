307b6929712d9a370e4fbe5644eac5368d6164d9
Douglas Utz <douglas@utz.net.br>
2024-08-13 23:03:41 -0300

    Test

--- api/app.py
+++ api/app.py
@@ -10,24 +10,24 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# Track product endpoint
-@app.route('/track/<path:url>', methods=['GET'])
-def get_product(url):
+# # Track product endpoint
+# @app.route('/track/<path:url>', methods=['GET'])
+# def get_product(url):
     
-    # Decode the URL
-    product_url = unquote(url)
+#     # Decode the URL
+#     product_url = unquote(url)
     
-    # Get the necessary data
-    name = get_name(product_url)
-    price = get_price(product_url)
-    date = get_date()
-    time = get_time()
+#     # Get the necessary data
+#     name = get_name(product_url)
+#     price = get_price(product_url)
+#     date = get_date()
+#     time = get_time()
     
-    # Create the dictionary (JSON)
-    product = {
-        'name': str(name),
-        'price': str(price),
-        'date': date,
-        'time': time
-    }
-    return jsonify(product)
+#     # Create the dictionary (JSON)
+#     product = {
+#         'name': str(name),
+#         'price': str(price),
+#         'date': date,
+#         'time': time
+#     }
+#     return jsonify(product)
